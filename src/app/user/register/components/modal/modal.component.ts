31a9b8fa42f389a82d4790a1967675ddc5e4fb37
lindsaymb15 <lindsaymorales15@gmail.com>
2020-07-07 20:41:39 -0600

    link to registration page using query params

--- src/app/user/register/components/modal/modal.component.ts
+++ src/app/user/register/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ModalComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goToRegistrationPage(userType: string) {
+    this.router.navigate(['/register'], {queryParams: {user: userType}});
+  }
 
 }
