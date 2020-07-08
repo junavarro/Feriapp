d9d5251a5f4ebb34aebcaca8d6281d87a0c058de
lindsaymb15 <lindsaymorales15@gmail.com>
2020-07-08 15:05:59 -0600

    registration page updated

--- src/app/user/register/register.page.ts
+++ src/app/user/register/register.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+
 export class RegisterPage implements OnInit {
   
-  constructor() { }
+  private userType: string;
+  private sellerRegistrationForm : FormGroup;
+  private buyerRegistrationForm : FormGroup;
 
-  ngOnInit(){
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.userType = params['user'];
+    });
+
+    if (this.userType=='seller') { 
+      this.sellerRegistrationForm = this.formBuilder.group({
+        phone:  new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(8),
+          Validators.pattern(/^-?(0|[1-9]\d*)?$/)])),
+      });
+    }
+
+    else {
+      this.buyerRegistrationForm = this.formBuilder.group({});
+    }
   }
 
+  registerSeller() {
+    console.log(this.sellerRegistrationForm.value)
+  }
+
+  registerBuyer() {
+    console.log(this.buyerRegistrationForm.value)
+  }
 }
