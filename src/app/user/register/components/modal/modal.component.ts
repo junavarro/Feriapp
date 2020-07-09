be0b7e25336e67adcd8518da9bc70c3fc68583d6
mapachass16 <mapachass-16@hotmail.com>
2020-07-08 21:18:11 -0600

    validation of user choice

--- src/app/user/register/components/modal/modal.component.ts
+++ src/app/user/register/components/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -8,10 +9,36 @@ import { Router } from '@angular/router';
 })
 export class ModalComponent {
 
-  constructor(private router: Router) { }
+  private selectedUserType: string;
+  private isSellerSelected: boolean;
+  private isBuyerSelected: boolean;
+
+  constructor(private router: Router, private modalCtrl: ModalController) { 
+    selectedUserType: "";
+    isSellerSelected: false;
+    isBuyerSelected: false;
+  }
 
   goToRegistrationPage(userType: string) {
+    this.dismiss();
     this.router.navigate(['/register'], {queryParams: {user: userType}});
   }
+
+  selectUserType(userType:string) {
+    this.selectedUserType = userType;
+    if (userType=="seller") {
+           this.isSellerSelected=true;
+           this.isBuyerSelected=false;
+    }
+    else {
+          this.isSellerSelected=false;
+          this.isBuyerSelected=true;
+    }
+ }
+ dismiss() {
+  this.modalCtrl.dismiss({
+    'dismissed': true
+  });
+}
 
 }
